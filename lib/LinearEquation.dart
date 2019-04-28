e298931851dca91d8b7b2f7c32ca1d532d956bf7
sunhang <sunhang09@gmail.com>
2019-04-28 15:17:46 +0800

    添加seekbar

--- lib/LinearEquation.dart
+++ lib/LinearEquation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:ui';
+import 'package:flutter_seekbar/flutter_seekbar.dart' show SeekBar;
 
 class LinearEquation extends StatelessWidget {
   @override
@@ -12,7 +13,23 @@ class LinearEquation extends StatelessWidget {
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.width,
           child: CustomPaint(painter: new LinearEquationPainter()),
-        )
+        ),
+        Padding(
+            padding:
+                new EdgeInsets.only(left: 20, top: 40, right: 20, bottom: 10),
+            child: SeekBar(
+              progresseight: 5,
+              value: 0.5,
+              sectionCount: 5,
+            )),
+        Padding(
+            padding:
+                new EdgeInsets.only(left: 20, top: 40, right: 20, bottom: 10),
+            child: SeekBar(
+              progresseight: 5,
+              value: 0.5,
+              sectionCount: 5,
+            ))
       ],
     );
   }
@@ -31,6 +48,8 @@ class LinearEquationPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final rect = Offset(0, 0) & size;
 
+    print("$rect");
+
     canvas.clipRect(rect);
 //    canvas.drawColor(Color.fromARGB(255, 255, 255, 255), BlendMode.srcIn);
 
@@ -47,48 +66,56 @@ class LinearEquationPainter extends CustomPainter {
     canvas.drawLine(p0, p1, _paint);
 
     for (double i = 0; i < size.width / 2; i += 40) {
-      canvas.drawLine(Offset(size.width / 2 + i, size.height / 2), Offset(size.width / 2 + i, size.height / 2 - 10), _paint);
-      canvas.drawLine(Offset(size.width / 2 - i, size.height / 2), Offset(size.width / 2 - i, size.height / 2 - 10), _paint);
+      canvas.drawLine(Offset(size.width / 2 + i, size.height / 2),
+          Offset(size.width / 2 + i, size.height / 2 - 10), _paint);
+      canvas.drawLine(Offset(size.width / 2 - i, size.height / 2),
+          Offset(size.width / 2 - i, size.height / 2 - 10), _paint);
     }
 
     for (double i = 0; i < size.height / 2; i += 40) {
-      canvas.drawLine(Offset(size.width / 2, size.height / 2 + i), Offset(size.width / 2 + 10, size.height / 2 + i), _paint);
-      canvas.drawLine(Offset(size.width / 2, size.height / 2 - i), Offset(size.width / 2 + 10, size.height / 2 - i), _paint);
+      canvas.drawLine(Offset(size.width / 2, size.height / 2 + i),
+          Offset(size.width / 2 + 10, size.height / 2 + i), _paint);
+      canvas.drawLine(Offset(size.width / 2, size.height / 2 - i),
+          Offset(size.width / 2 + 10, size.height / 2 - i), _paint);
     }
 
     final arrowSize = 15.0;
     // 画X轴箭头
-    canvas.drawLine(Offset(size.width, size.height / 2), Offset(size.width - arrowSize, size.height / 2 - arrowSize), _paint);
-    canvas.drawLine(Offset(size.width, size.height / 2), Offset(size.width - arrowSize, size.height / 2 + arrowSize), _paint);
+    canvas.drawLine(Offset(size.width, size.height / 2),
+        Offset(size.width - arrowSize, size.height / 2 - arrowSize), _paint);
+    canvas.drawLine(Offset(size.width, size.height / 2),
+        Offset(size.width - arrowSize, size.height / 2 + arrowSize), _paint);
 
     // 画Y轴箭头
-    canvas.drawLine(Offset(size.width / 2, 0), Offset(size.width / 2 - arrowSize, arrowSize), _paint);
-    canvas.drawLine(Offset(size.width / 2, 0), Offset(size.width / 2 + arrowSize, arrowSize), _paint);
+    canvas.drawLine(Offset(size.width / 2, 0),
+        Offset(size.width / 2 - arrowSize, arrowSize), _paint);
+    canvas.drawLine(Offset(size.width / 2, 0),
+        Offset(size.width / 2 + arrowSize, arrowSize), _paint);
 
     // 绘制O
     TextSpan span = new TextSpan(
         style: new TextStyle(color: Colors.black, fontSize: 20.0), text: "O");
-    TextPainter tp = new TextPainter(
-        text: span, textDirection: TextDirection.ltr);
+    TextPainter tp =
+        new TextPainter(text: span, textDirection: TextDirection.ltr);
     tp.layout();
     tp.paint(canvas, new Offset(size.width / 2 - tp.width, size.height / 2));
 
     // 绘制X
     TextSpan spanX = new TextSpan(
         style: new TextStyle(color: Colors.black, fontSize: 20.0), text: "X");
-    TextPainter tpX = new TextPainter(
-        text: spanX, textDirection: TextDirection.ltr);
+    TextPainter tpX =
+        new TextPainter(text: spanX, textDirection: TextDirection.ltr);
     tpX.layout();
-    tpX.paint(canvas, new Offset(size.width - tpX.width, size.height / 2 + arrowSize));
+    tpX.paint(canvas,
+        new Offset(size.width - tpX.width, size.height / 2 + arrowSize));
 
     // 绘制Y
     TextSpan spanY = new TextSpan(
         style: new TextStyle(color: Colors.black, fontSize: 20.0), text: "Y");
-    TextPainter tpY = new TextPainter(
-        text: spanY, textDirection: TextDirection.ltr);
+    TextPainter tpY =
+        new TextPainter(text: spanY, textDirection: TextDirection.ltr);
     tpY.layout();
     tpY.paint(canvas, new Offset(size.width / 2 - tpY.width - arrowSize, 0));
-
 
     _paint.color = Colors.blue;
     double y;
